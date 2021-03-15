93e777cfd17b5f6b101bde3e19e6fe9ba257876d
Tejas Sadashiv <Tejas.sadashivaiah@gmail.com>
2021-03-14 22:32:14 -0700

    First commit!

---
+++ src/com/freecodecamp/javalessions/lessons/Main.java
@@ -0,0 +1,9 @@
+package com.freecodecamp.javalessions.lessons;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+	System.out.println("Hello World! with Git");
+    }
+}
